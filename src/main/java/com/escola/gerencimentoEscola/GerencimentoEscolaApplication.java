11a662de3b3413946b0965ca742bc69aa2418f2b
laptopfatec <fatec@teste.com>
2024-10-30 13:59:21 -0300

    initial commit

---
+++ src/main/java/com/escola/gerencimentoEscola/GerencimentoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.escola.gerencimentoEscola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencimentoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencimentoEscolaApplication.class, args);
+	}
+
+}
