50b17a42e888cb045a6bf8ce9c70a66320234a56
laptopfatec <fatec@teste.com>
2024-10-30 14:18:07 -0300

    add something

---
+++ src/main/java/com/escola/gerencimentoEscola/model/Aluno.java
@@ -0,0 +1,79 @@
+package com.escola.gerencimentoEscola.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.repository.Repository;
+
+@Entity
+public class ClasseDisciplina() {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private double nota;
+}
+
+@Entity
+public class Aluno {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String matricula;
+    private int idade;
+    private String turma;
+
+    public Aluno(){}
+
+    public Aluno(String matricula, int idade, String turma) {
+        this.matricula = matricula;
+        this.idade = idade;
+        this.turma = turma;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public double calcularMedia() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", matricula='" + matricula + '\'' +
+                ", idade=" + idade +
+                ", turma='" + turma + '\'' +
+                '}';
+    }
+}
+
+interface AlunoReposotory extends Repository<Aluno, Long> {
+
+}
